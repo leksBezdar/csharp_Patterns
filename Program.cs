3c64e692020366d7b125cdae3790907e9c3909ac
leksBezdar <vollkovalex1991@mail.ru>
2024-09-04 10:13:33 +0400

    Add tests and namespaces

--- Program.cs
+++ Program.cs
@@ -1,228 +1,262 @@
 ﻿using System.Runtime.InteropServices;
 
-public class Singleton<T> where T: class, new()
+namespace SingletonPattern
 {
-    private static T? instance = null;
-    private static object lockObject = new object();
-    protected Singleton() { }
-
-    public static T Instance
+    public class Singleton<T> where T : class, new()
     {
-        get
+        private static T? instance = null;
+        private static readonly object lockObject = new object();
+
+        protected Singleton() { }
+
+        public static T Instance
         {
-            lock (lockObject)
+            get
             {
-                if (instance == null)
+                lock (lockObject)
                 {
-                    instance = new T();
+                    if (instance == null)
+                    {
+                        instance = new T();
+                    }
+                    return instance;
                 }
-                return instance;
-            } 
-        }
-    }
-}
-
-public class SingletonObject : Singleton<SingletonObject>
-{
-    private GCHandle? objectID = null;
-
-    public GCHandle ObjectID
-    {
-        get
-        {
-            if (objectID == null)
-            {
-                objectID = GCHandle.Alloc(Instance, GCHandleType.Pinned);
             }
-            return objectID.Value;
         }
     }
 
-    public void Free()
+    public class SingletonObject : Singleton<SingletonObject>
     {
-        if (objectID.HasValue && objectID.Value.IsAllocated)
+        private GCHandle? objectID = null;
+
+        public GCHandle ObjectID
         {
-            objectID.Value.Free();
-            objectID = null;
-        }
-    }
-}
-
-
-public class EventBroker
-{
-    private readonly Dictionary<string, List<Action<object>>> _subscribers = new();
-
-    public void subscribe(string eventName, Action<object> action)
-    {
-        if (!_subscribers.ContainsKey(eventName))
-        {
-            _subscribers[eventName] = new List<Action<object>>();
-        }
-
-        _subscribers[eventName].Add(action);
-    }
-
-    public void unsubscribe(string eventName, Action<object> action)
-    {
-        if (_subscribers.ContainsKey(eventName))
-        {
-            _subscribers[eventName].Remove(action);
-        }
-    }
-
-    public void Publish(string eventName, object eventData )
-    {
-        if (_subscribers.ContainsKey(eventName))
-        {
-            foreach (var action in _subscribers[eventName])
+            get
             {
-                action(eventData);
+                if (objectID == null)
+                {
+                    objectID = GCHandle.Alloc(Instance, GCHandleType.Pinned);
+                }
+                return objectID.Value;
+            }
+        }
+
+        public void Free()
+        {
+            if (objectID.HasValue && objectID.Value.IsAllocated)
+            {
+                objectID.Value.Free();
+                objectID = null;
             }
         }
     }
 }
 
-public class Subscriber
+namespace EventBrokerPattern
 {
-    public Subscriber(EventBroker eventBroker)
+    public class EventBroker
     {
-        eventBroker.subscribe("eventOccured", OnEventOccured);
-    }
+        private readonly Dictionary<string, List<Action<object>>> _subscribers = new();
 
-    private void OnEventOccured(object eventData)
-    {
-        Console.WriteLine($"Subscriber: Event data recieved: {eventData}");
-    }
-}
-
-public class Publisher
-{
-    private readonly EventBroker _eventBroker;
-
-    public Publisher(EventBroker eventBroker)
-    {
-        _eventBroker = eventBroker;
-    }
-
-    public void PublishEvent()
-    {
-        _eventBroker.Publish("eventOccured", "Hello, World!");
-    }
-}
-
-
-public interface IOldPaymentSystem
-{
-    void ProcessPayment(double amount);
-}
-
-public interface INewPaymentSystem
-{
-    void ProcessPayment(double amount, string currency);
-}
-
-public class OldPaymentSystem : IOldPaymentSystem
-{
-    public void ProcessPayment(double amount)
-    {
-        Console.WriteLine($"Processing payment of {amount} with an old system logic");
-    }
-
-}
-
-public class NewPaymentSystem : INewPaymentSystem
-{
-    public void ProcessPayment(double amount, string currency)
-    {
-        Console.WriteLine($"Processing payment of {amount} {currency} with a new system logic");
-    }
-}
-
-public class PaymentSystemAdapter : IOldPaymentSystem
-{
-    private readonly INewPaymentSystem _newPaymentSystem;
-    private readonly string _defaultCurrency;
-
-    public PaymentSystemAdapter(INewPaymentSystem newPaymentSystem, string defaultCurrency)
-    {
-        _newPaymentSystem = newPaymentSystem;
-        _defaultCurrency = defaultCurrency;
-    }
-
-    public void ProcessPayment(double amount)
-    {
-        _newPaymentSystem.ProcessPayment(amount, _defaultCurrency);
-    }
-
-    public static void Perform(double amount, string currency)
-    {
-        INewPaymentSystem newPaymentSystem = new NewPaymentSystem();
-        PaymentSystemAdapter paymentSystemAdapter = new(newPaymentSystem, currency);
-
-        paymentSystemAdapter.ProcessPayment(amount);
-    }
-}
-
-
-public class StringHelper
-{
-    public static bool IsPalindrome(string input)
-    {
-        if (string.IsNullOrEmpty(input)) 
+        public void Subscribe(string eventName, Action<object> action)
         {
-            return false;
+            if (!_subscribers.ContainsKey(eventName))
+            {
+                _subscribers[eventName] = new List<Action<object>>();
+            }
+
+            _subscribers[eventName].Add(action);
         }
-        string reversedString = new string(input.Reverse().ToArray());
-        return input.Equals(reversedString, StringComparison.OrdinalIgnoreCase);
+
+        public void Unsubscribe(string eventName, Action<object> action)
+        {
+            if (_subscribers.ContainsKey(eventName))
+            {
+                _subscribers[eventName].Remove(action);
+            }
+        }
+
+        public void Publish(string eventName, object eventData)
+        {
+            if (_subscribers.ContainsKey(eventName))
+            {
+                foreach (var action in _subscribers[eventName])
+                {
+                    action(eventData);
+                }
+            }
+        }
     }
 
-    public static Dictionary<char, int> GetCharacterFrequency(string input)
+    public class Subscriber
     {
-        var frequency = new Dictionary<char, int>();
+        public Subscriber(EventBroker eventBroker)
+        {
+            eventBroker.Subscribe("eventOccured", OnEventOccured);
+        }
 
-        if (string.IsNullOrEmpty(input)) {
+        private void OnEventOccured(object eventData)
+        {
+            Console.WriteLine($"Subscriber: Event data received: {eventData}");
+        }
+    }
+
+    public class Publisher
+    {
+        private readonly EventBroker _eventBroker;
+
+        public Publisher(EventBroker eventBroker)
+        {
+            _eventBroker = eventBroker;
+        }
+
+        public void PublishEvent()
+        {
+            _eventBroker.Publish("eventOccured", "Hello, World!");
+        }
+    }
+}
+
+namespace AdapterPattern
+{
+    public interface IOldPaymentSystem
+    {
+        void ProcessPayment(double amount);
+    }
+
+    public interface INewPaymentSystem
+    {
+        void ProcessPayment(double amount, string currency);
+    }
+
+    public class OldPaymentSystem : IOldPaymentSystem
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Processing payment of {amount} with an old system logic");
+        }
+    }
+
+    public class NewPaymentSystem : INewPaymentSystem
+    {
+        public void ProcessPayment(double amount, string currency)
+        {
+            Console.WriteLine($"Processing payment of {amount} {currency} with a new system logic");
+        }
+    }
+
+    public class PaymentSystemAdapter : IOldPaymentSystem
+    {
+        private readonly INewPaymentSystem _newPaymentSystem;
+        private readonly string _defaultCurrency;
+
+        public PaymentSystemAdapter(INewPaymentSystem newPaymentSystem, string defaultCurrency)
+        {
+            _newPaymentSystem = newPaymentSystem;
+            _defaultCurrency = defaultCurrency;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            _newPaymentSystem.ProcessPayment(amount, _defaultCurrency);
+        }
+
+        public static void Perform(double amount, string currency)
+        {
+            INewPaymentSystem newPaymentSystem = new NewPaymentSystem();
+            PaymentSystemAdapter paymentSystemAdapter = new(newPaymentSystem, currency);
+
+            paymentSystemAdapter.ProcessPayment(amount);
+        }
+    }
+}
+
+namespace HelperPattern
+{
+    public class StringHelper
+    {
+        public static bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            string reversedString = new string(input.Reverse().ToArray());
+            return input.Equals(reversedString, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static Dictionary<char, int> GetCharacterFrequency(string input)
+        {
+            var frequency = new Dictionary<char, int>();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return frequency;
+            }
+
+            foreach (char c in input)
+            {
+                if (frequency.ContainsKey(c))
+                {
+                    frequency[c]++;
+                }
+                else
+                {
+                    frequency[c] = 1;
+                }
+            }
             return frequency;
-        };
-
-        foreach(char c in input)
-        {
-            if (frequency.ContainsKey(c))
-            {
-                frequency[c]++;
-            }
-            else
-            {
-                frequency[c] = 1;
-            }
         }
-        return frequency;
     }
 }
 
 class Program
 {
-    SingletonObject singletonObject1 = SingletonObject.Instance;
-    SingletonObject singletonObject2 = SingletonObject.Instance;
-    static void Main(string[] args)
+    static void Main()
     {
-        Program program = new();
-        
-        IntPtr addres1 = program.singletonObject1.ObjectID.AddrOfPinnedObject();
-        IntPtr addres2 = program.singletonObject2.ObjectID.AddrOfPinnedObject();
-        Console.WriteLine($"{addres1} == {addres2}: {addres1 == addres2}");
+        TestSingletonPattern();
+        TestEventBrokerPattern();
+        TestAdapterPattern();
+        TestHelperPattern();
 
-        program.singletonObject1.Free();
-        program.singletonObject2.Free();
-
-        EventBroker eventBroker = new();
-        Publisher publisher = new(eventBroker);
-        Subscriber subscriber = new(eventBroker);
-
-        //publisher.PublishEvent();
-
-
-        //PaymentSystemAdapter.Perform(1000, "Rub");
         Console.ReadKey();
+    }
+
+    private static void TestSingletonPattern()
+    {
+        var singletonObject1 = SingletonPattern.SingletonObject.Instance;
+        var singletonObject2 = SingletonPattern.SingletonObject.Instance;
+
+        IntPtr address1 = singletonObject1.ObjectID.AddrOfPinnedObject();
+        IntPtr address2 = singletonObject2.ObjectID.AddrOfPinnedObject();
+        Console.WriteLine($"{address1} == {address2}: {address1 == address2}");
+        singletonObject1.Free();
+        singletonObject2.Free();
+    }
+
+    private static void TestEventBrokerPattern()
+    {
+        var eventBroker = new EventBrokerPattern.EventBroker();
+        var publisher = new EventBrokerPattern.Publisher(eventBroker);
+        var subscriber = new EventBrokerPattern.Subscriber(eventBroker);
+        publisher.PublishEvent();
+    }
+
+    private static void TestAdapterPattern()
+    {
+        AdapterPattern.PaymentSystemAdapter.Perform(1000, "Rub");
+    }
+
+    private static void TestHelperPattern()
+    {
+        Console.WriteLine($" Слово 'кабак' - это палиндром? {HelperPattern.StringHelper.IsPalindrome("кабак")}");
+        Console.WriteLine($" Слово 'палиндром' - это палиндром? {HelperPattern.StringHelper.IsPalindrome("палиндром")}");
+
+        var frequency = HelperPattern.StringHelper.GetCharacterFrequency("Тетрагидропиранилциклопентилтетрагидропиридопиридиновые");
+        Console.WriteLine("Частота символов:");
+        foreach (var kvp in frequency)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
     }
 }
